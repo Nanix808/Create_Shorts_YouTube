c41ab6be74d3f971da3e15f33d002e5e4e96f3e2
savosinvitali <savosinvitali@gmail.com>
2024-01-12 14:24:46 +0300

    created folders

---
+++ text_to_audio/text_to_audio.py
@@ -0,0 +1,49 @@
+from elevenlabs import generate, play, voices, save
+
+from config import API_KEY
+from interface_text_to_audio import TextToAudio
+
+QUESTION = "Привет"
+ANSWER = "Привет"
+
+
+
+
+class TextToAudioElevenlabs(TextToAudio):
+    def __init__(self, questions: list, answers: list, api_key: str) -> None:
+        self.questions = questions
+        self.answers = answers
+        self.api_key = api_key
+        self.voices = voices()
+
+    def text_to_audio(self):
+        for question in self.questions:
+            audio = generate(
+                api_key=self.api_key,
+                text=question,
+                voice=self.voices[38],
+                model="eleven_multilingual_v2",
+            )
+            name = "question_" + question[:5]
+            self.save_audio_to_mp3(audio, name)
+
+        for answer in self.answers:
+            audio = generate(
+                api_key=self.api_key,
+                text=answer,
+                voice=self.voices[38],
+                model="eleven_multilingual_v2",
+            )
+            name = "answer_" + answer[:5]
+            return self.save_audio_to_mp3(audio, name)
+
+    def save_audio_to_mp3(self, audio, name):
+        full_name = f"download_audio/{name}.mp3"
+        save(audio, f"download_audio/{full_name}")
+        return full_name
+
+    def play_audio(self, audio):
+        play(audio)
+
+
+TextToAudioElevenlabs([QUESTION], [ANSWER], API_KEY).text_to_audio()
