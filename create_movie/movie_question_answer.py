5a3f64a78d1e01e629f751a499286fa18af7906f
savosinvitali <savosinvitali@gmail.com>
2024-01-15 16:12:29 +0300

    added save video

--- create_movie/movie_question_answer.py
+++ create_movie/movie_question_answer.py
@@ -1,9 +1,6 @@
 from create_movie.interface_create_movie import CreateMovie
 from moviepy.editor import *
 
-# QUESTION = "У ног кузовок: Кто за ней ходит, того она и кормит. Два бодаста, четыре ходаста, Один хлестун"
-# ANSWER = "Корова"
-
 
 class CreateVideoMoviepy(CreateMovie):
     def __init__(
@@ -25,27 +22,36 @@ class CreateVideoMoviepy(CreateMovie):
         self.background_video = background_video
         self.background_audio = background_audio
         self.name_chanel = name_chanel
+        self.countainer_movie = []
 
     def create_video(self):
-        audioclip = AudioFileClip(f"{self.audio_question}")
-        audioclip_answer = AudioFileClip(f"{self.audio_answer}")
-        duration = audioclip.duration
+        # Create audioclips questions and answer
+        audioclip_question = AudioFileClip(self.audio_question)
+        audioclip_answer = AudioFileClip(self.audio_answer)
 
-        audioclip = audioclip.set_duration(duration - 0.1)
+        # find out the duration of the question
+        duration = audioclip_question.duration
 
+        # remove artifact audio of the questions
+        audioclip_question = audioclip_question.set_duration(duration - 0.1)
+
+        # create backround videoclip
         video = (
             VideoFileClip(self.background_video)
-            .fx(vfx.resize, width=1080)  # resize
-            .fx(vfx.resize, height=1920)  # resize
-            .fx(vfx.loop)  # loop
+            .fx(vfx.resize, width=1080)
+            .fx(vfx.resize, height=1920)
+            .fx(vfx.loop)
             .set_duration(duration + self.countback + 5)
-            # .audio_fadeout(20)
         )
-        video.audio = audioclip
+
+        # add backround audio
         video.audio = AudioFileClip(self.background_audio).set_duration(
             duration + self.countback + 5
         )
+
+        # make background music quieter
         video = video.volumex(0.3)
+
         # Create countdown timer
         cliplist = []
         for time in range(self.countback, 0, -1):
@@ -64,7 +70,7 @@ class CreateVideoMoviepy(CreateMovie):
             )
             cliplist.append(countdown_clip)
 
-        # Make the text. Many more options are available.
+        # Make the text of question
         txt_question = (
             TextClip(
                 self.question,
@@ -75,10 +81,29 @@ class CreateVideoMoviepy(CreateMovie):
                 size=(1000, 1400),
             )
             .set_position((40, 60))
-            .set_duration(duration + 7)
+            .set_duration(duration + self.countback)
         )
-        txt_question.audio = audioclip.set_start(1)
 
+        # add audioclip_question
+        txt_question.audio = audioclip_question.set_start(1)
+
+        # Make the text of answer
+        txt_answer = (
+            TextClip(
+                self.answer,
+                font="Arial",
+                fontsize=150,
+                color="white",
+                method="caption",
+                size=(1000, 1400),
+            )
+            .set_position((40, 60))
+            .set_duration(4.5)
+            .set_start(duration + self.countback + 0.5)
+        )
+        txt_answer.audio = audioclip_answer.set_start(duration + self.countback + 0.5)
+
+        # Make the text of chanel
         txt_chanel = (
             TextClip(
                 self.name_chanel,
@@ -92,26 +117,11 @@ class CreateVideoMoviepy(CreateMovie):
             .set_duration(duration + self.countback + 5)
         )
 
-        txt_answer = (
-            TextClip(
-                self.answer,
-                font="Arial",
-                fontsize=150,
-                color="white",
-                method="caption",
-                size=(1000, 1400),
-            )
-            .set_position((40, 60))
-            .set_duration(4.5)
-            .set_start(duration + 7.5)
-        )
-        txt_answer.audio = audioclip_answer.set_start(duration + 7.5)
-
-        result = CompositeVideoClip(
+        self.countainer_movie = CompositeVideoClip(
             [video, txt_question, txt_answer, txt_chanel] + cliplist
-        )  # Overlay text on video
-        # result.preview()
-        result.write_videofile(f"download/{self.question[:99]}.mp4")  # Many options...
+        )
 
+        return self
 
-# CreateVideoMoviepy(QUESTION, ANSWER, "У ног кузо.mp3", "Корова..mp3").create_video()
+    def save_video(self):
+        self.countainer_movie.write_videofile(f"download/{self.question[:99]}.mp4")
